c82076bb2d3a79a31b5721c4d039921417063e06
Elsa Martinez <elsa.martinez.faucard@gmail.com>
2018-11-12 10:28:23 +0100

    profile service made

--- routes/user.js
+++ routes/user.js
@@ -3,29 +3,29 @@ const router = express.Router();
 
 const User = require('../models/user');
 
-// router.get('/profile', isLoggedIn, (req, res) => {
-//   const userId = req.params.id;
-//   User.findById(userId)
-//     .then(user => {
-//       return res.json(user);
-//     })
-//     .catch(error => {
-//       return res.json(error);
-//     } );
-// });
+router.get('/profile', isLoggedIn, (req, res) => {
+  const userId = req.params.id;
+  User.findById(userId)
+    .then(user => {
+      return res.json(user);
+    })
+    .catch(error => {
+      return res.json(error);
+    } );
+});
 
-// router.post('/profile/create', (req, res, next) => {
-//     User.create({
-//       description: req.body.description,
-//       email: req.body.email,
-//       styles: [req.body.styles]
-//     }).then((response) => {
-//       res.status(200).json(response)
-//     }).catch((error) => {
-//       next(error);
-//     })
+router.post('/profile/create', (req, res, next) => {
+    User.findById(userId)({
+      description: req.body.description,
+      email: req.body.email,
+      styles: [req.body.styles]
+    }).then((response) => {
+      res.status(200).json(response)
+    }).catch((error) => {
+      next(error);
+    })
 
-// })
+})
 
 
 module.exports = router;
